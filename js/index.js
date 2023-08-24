3bfa6c1fe096b88a20a5a3859a3e62492385222c
Cristian-000 <fcristian480@gmail.com>
2023-08-24 14:42:34 -0300

    pauta2, agregando catName

--- js/index.js
+++ js/index.js
@@ -1,14 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const categoryElements = document.querySelectorAll(".custom-card[id]");
+
+    categoryElements.forEach(function(element) {
+        element.addEventListener("click", function() {
+            const categoryName = element.getAttribute("id");
+            localStorage.setItem("catID", categoryName);
+            window.location = "products.html";
+        });
+    });
+});
+/*
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
+        localStorage.setItem("catID", "autos");
         window.location = "products.html"
     });
     document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
+        localStorage.setItem("catID", "juguetes");
         window.location = "products.html"
     });
     document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
+        localStorage.setItem("catID", "muebles");
         window.location = "products.html"
     });
 });
+*/
