26e43f9170a097937715af6f77eae1a0befa56d1
Cristian-000 <129860008+Cristian-000@users.noreply.github.com>
2023-09-12 17:53:30 -0300

    comit1

--- js/product-info.js
+++ js/product-info.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         productName.innerHTML = dataArrayProduct.name;
         productDescription.innerHTML = dataArrayProduct.description;
         productPrice.innerHTML = dataArrayProduct.cost;
+        
         for (const item of dataArrayProduct.images) {
 
             const imagenDelProducto = document.createElement("img");
@@ -22,19 +23,44 @@ document.addEventListener("DOMContentLoaded", async function () {
             imagenDelProducto.classList.add("imagen-del-producto");
             productImages.appendChild(imagenDelProducto);
         }
+    }   
+    function showComents(dataComents) {
+      
+        dataComents.forEach(comment => {
+            const commentDiv = document.createElement('div');
+    
+            // Agrega contenido HTML al div del comentario usando los datos del JSON.
+            commentDiv.innerHTML = `
+                <h3>${comment.user}</h3>
+                <p>Calificación: ${comment.score}</p>
+                <p>${comment.description}</p>
+                <p>Fecha y Hora: ${comment.dateTime}</p>
+            `;
+    
+            // Agrega el div del comentario al contenedor 'productComments'.
+            productComments.appendChild(commentDiv);
+        });
     }
-
-    const urlProduct = `https://japceibal.github.io/emercado-api/products/${productID}.json`
-
+    
     try {
-        const response = await fetch(urlProduct);
-        const data = await response.json();
-        originalData = data; // Almacenar los datos originales
-        showDataProduct(data);
-    } catch (error) {
-        console.error("Error trayendo:", error);
-    }
+        const urlProduct = `https://japceibal.github.io/emercado-api/products/${productID}.json`;
+        const urlComent = `https://japceibal.github.io/emercado-api/products_comments/${productID}.json`;
 
+        // Realizamos ambas solicitudes HTTP al mismo tiempo usando Promise.all
+        const [productResponse, commentsResponse] = await Promise.all([
+            fetch(urlProduct),
+            fetch(urlComent)
+        ]);
+
+        const productData = await productResponse.json();
+        const commentsData = await commentsResponse.json();
+
+        // Llamamos a las funciones para mostrar los datos del producto y los comentarios.
+        showDataProduct(productData);
+        showComents(commentsData);
+    } catch (error) {
+        console.error("Error trayendo datos:", error);
+    }
 })
 
 
