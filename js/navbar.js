9f78f1cfd18ca221264ec6ebcb14a7a8f92cd13d
Esteban Bentancor <ebentancor341@gmail.com>
2023-09-29 20:33:21 -0300

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -51,24 +51,24 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     // funcion para cambiar el color de fondo de la pagina //
 
-    const darkmode = document.getElementById('darkmode');
-    const body = document.body;
-    let status = false;
+    const darkmode = document.getElementById('darkmode'); // creamos una constante que toma el id del boton darkmode
+    const body = document.body; // guardamos en otra variable el body
+    let estadoDeDarkmode = false;
     
     // Verificar si el modo oscuro está habilitado en el almacenamiento local
-    const savedStatus = localStorage.getItem('status');
+    const savedStatus = localStorage.getItem('EstadoDeDarkMode');
     if (savedStatus === 'true') {
-        status = true;
+        estadoDeDarkmode = true;
         body.classList.add('dark-mode'); // Agrega la clase 'dark-mode' si el modo oscuro está habilitado
     }
     
     darkmode.addEventListener('change', function () {
         // Invierte el estado y actualiza el almacenamiento local
-        status = !status;
-        localStorage.setItem('status', status);
+        estadoDeDarkmode = !estadoDeDarkmode;
+        localStorage.setItem('estadoDarkMode', estadoDeDarkmode);
     
         // Aplica o retira la clase 'dark-mode' según el estado
-        if (status) {
+        if (estadoDeDarkmode) {
             body.classList.add('dark-mode');
         } else {
             body.classList.remove('dark-mode');
