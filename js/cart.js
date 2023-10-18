15e7bacfbe21a1e3f8146c4b1267fb9c8cd3daa2
Lucas G <gonzalezmoraleslucas@gmail.com>
2023-10-18 00:32:29 -0300

    primero cambios lucas

--- js/cart.js
+++ js/cart.js
@@ -110,5 +110,153 @@ document.addEventListener("DOMContentLoaded", async function () {
         // Mostrar los totales por separado en elementos HTML solo si no son cero
         totalAmountElement.textContent = `${totalUSDString} ${totalUYUString} ${totalCarritoEsCero}`;
     }
-  
+
+    // inicio de la pauta 6 entrega 2 //
+    function validateForm(){
+ 
+        let texto = document.getElementById("tipoPago").outerText;
+        
+        if(texto === "No se selecciono metodo de pago"){
+            document.getElementById("validacionMetodoPago").style.display= "block"
+        }else{
+    
+            let tipoPago =  document.getElementById("tipoPago").outerText;
+    
+            if (tipoPago === "Tarjeta de Credito") {
+    
+                let resultNumeroTarjeta = document.getElementById("resultNumeroTarjeta");
+                let resultnombreTitular = document.getElementById("resultnombreTitular");
+                let resultnombreMonthYear = document.getElementById("resultnombreMonthYear");
+                let resultModalCvc = document.getElementById("resultModalCvc");
+            
+                    if (resultNumeroTarjeta.value != "" && resultnombreTitular.value != "" && resultnombreMonthYear.value != "" && resultModalCvc.value != "") {
+                    showAlert();
+                    }else{
+                        document.getElementById("alert").innerHTML = `<p>Atencion! falta ingresar datos de la tarjeta decredito</p>`
+                    }
+            }else{
+    
+                let resultModalnumeroCuenta = document.getElementById("resultModalnumeroCuenta");
+                let resultnombreTitular = document.getElementById("resultnombreTitular");
+    
+                if (resultModalnumeroCuenta.value != "" && resultnombreTitular.value != "") {
+                    showAlert();
+                    }else{
+                        document.getElementById("alert").innerHTML = `<p>Atencion! falta ingresar datos de la tarjeta decredito</p>`
+                    }
+            }
+    
+        }
+        
+    }
+    
+    function insertPaymentMethod( b ) {
+        
+        document.getElementById("validacionMetodoPago").style.display= "none"
+        let nombreTitular;
+        let numeroTarjeta;
+        let monthYear;
+        let cvc ;
+        let numeroCuenta;
+        let idButton = b.id;
+        let cardwrapper;
+    
+        let data;
+    
+        if (idButton === "completarTarjeta") {
+            nombreTitular = document.getElementById("modalName").value;
+            numeroTarjeta = document.getElementById("modalNumberCard").value;
+            monthYear = document.getElementById("modalMonthYear").value;
+            cvc = document.getElementById("modalCvc").value;
+            cardwrapper = document.getElementById("wrapper");
+            newcardwrapper = cardwrapper.innerHTML
+            
+    
+            data = `
+            
+            <h5 class="mb-2" id="tipoPago">Tarjeta de Credito</h5>
+            
+            <div class="card-body">
+    
+            <div class='card-wrapper' id="newCardWrapper"></div>
+    
+            
+              <div class="row mt-5">
+                <div class="form-group col-sm-6">
+                <label for="resultNumeroTarjeta">Numero</label>
+                  <input class="form-control form-control-rounded" id="resultNumeroTarjeta" placeholder="Numero de Tarjeta"
+                    type="text" name="number" value="`+ numeroTarjeta +`" required readonly>
+                </div>
+                <div class="form-group col-sm-6">
+                <label for="resultnombreTitular">Nombre</label>
+                  <input class="form-control form-control-rounded" id="resultnombreTitular" placeholder="Nombre Titular"
+                    type="text" name="name" value="`+ nombreTitular +`" required readonly/>
+                </div>
+                <div class="form-group col-sm-3">
+                <label for="resultnombreMonthYear">MM/YY</label>
+                  <input class="form-control form-control-rounded" placeholder="MM/YY" type="text"
+                    name="expiry" id="resultnombreMonthYear" value="`+ monthYear +`" required readonly/>
+                </div>
+                <div class="form-group col-sm-3">
+                  <label for="resultnombreTitular">CVC</label>
+                  <input class="form-control form-control-rounded" id="resultModalCvc" placeholder="CVC" type="text"
+                    name="cvc" value="`+ cvc +`" required readonly/>
+                </div>
+              </div>
+            
+            </div>
+            
+            <div id="alerta"></div>`
+    
+            document.getElementById("contMetodoEnvio").innerHTML = data;
+    
+            document.getElementById("newCardWrapper").innerHTML = newcardwrapper;
+            
+        } else {
+    
+            nombreTitular = document.getElementById("modalNameAcount").value;
+            numeroCuenta = document.getElementById("modalNumberAcount").value;
+    
+            data = `
+            
+            <div class="card-body">
+               <h3 class="text-center" id="tipoPago"><i class="fas fa-university"></i> Datos Cuenta bancaria </h3>
+               <div class="row">
+                 <div class="form-group col-sm-6">
+                 <label for="resultnombreTitular">Nombre Cuenta</label>
+                   <input class="form-control form-control-rounded" placeholder="Nombre"
+                     type="text" name="text" id="resultnombreTitular" value="`+ nombreTitular +`" required readonly>
+                 </div>
+                 <div class="form-group col-sm-6">
+                 <label for="resultModalnumeroCuenta">Numero Cuenta</label>
+                   <input class="form-control form-control-rounded" placeholder="Numero de Cuenta"
+                     type="number" name="name" id="resultModalnumeroCuenta" value="`+ numeroCuenta +`" readonly required>
+                </div>
+            </div>
+    
+            <div id="alerta"></div>`
+            
+            document.getElementById("contMetodoEnvio").innerHTML = data;
+        }
+    
+    }
+    
+    function showcard() {
+        
+        var card = new Card({
+            form: 'form',
+            container: '.card-wrapper',
+      
+            placeholders: {
+              number: '**** **** **** ****',
+              name: 'Nombre Apellido',
+              expiry: '**/**',
+              cvc: '***'
+            }
+          });
+    
+     
+    }
+    // fin de la entrega 6 pauta 2 //
+
 });
