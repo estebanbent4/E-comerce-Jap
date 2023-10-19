2e5e0eaefa45779bc4dd31dc6194f267a847f984
camilaprincipe <141370399+camilaprincipe@users.noreply.github.com>
2023-10-19 00:12:11 -0300

    mostrar pop up

--- js/cart.js
+++ js/cart.js
@@ -112,6 +112,12 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
 
     // inicio de la pauta 6 entrega 2 //
+   
+   
+
+      // Inicializa los modales de Bootstrap
+var myModal = new bootstrap.Modal(document.getElementById("modalMetodoPago"));
+
     function validateForm(){
  
         let texto = document.getElementById("tipoPago").outerText;
@@ -257,6 +263,24 @@ document.addEventListener("DOMContentLoaded", async function () {
     
      
     }
+
+    function () {
+        'use strict';
+        window.addEventListener('load', function () {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.getElementsByClassName('needs-validation');
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+            }, false);
+          });
+        }, false);
+      }
     // fin de la entrega 6 pauta 2 //
 
 });
